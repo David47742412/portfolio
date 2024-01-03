bf5daf7ac0a6abd8034728df9e9b5de1314ed819
David Castro <davidcastroastudillo30@mail.com>
2024-01-03 10:59:15 -0500

    home basic

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.X99eFLUw.js","_app/immutable/chunks/scheduler.m2oT1KzE.js","_app/immutable/chunks/index.KaB6U251.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.tX53hmWx.js","_app/immutable/chunks/scheduler.m2oT1KzE.js","_app/immutable/chunks/index.KaB6U251.js","_app/immutable/chunks/singletons.ciwKXHRS.js","_app/immutable/chunks/index.0yHvoc4R.js"];
-export const stylesheets = [];
-export const fonts = [];
